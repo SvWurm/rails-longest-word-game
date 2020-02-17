64601f11d05611df12f2e5c3841d7198dfe452be
svenia wurm <svwu@outlook.com>
2020-02-17 17:16:45 +0100

    At least I started

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  post 'games/score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'games/new', to: 'games#new', as: :new
+  # post 'games/score', to: 'games#score', as: :new
 end
